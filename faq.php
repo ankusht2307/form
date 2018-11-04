5ffe8a1b8cf2c347c967cd63113af0e3e4651a68
ankusht2307 <ankusht2307@gmail.com>
2018-11-04 15:55:07 +0530

    changes

--- faq.php
+++ faq.php
@@ -1,15 +1,28 @@
 <?php
-echo "<pre>If this line isn't printed you have a parse error in your script</pre>\n"; flush();
-error_reporting(E_ALL); ini_set('display_errors', true);    
-extension_loaded('pgsql') || die('pgsql module unavailable');
-
 
 $connection = pg_connect("host=ec2-23-21-171-249.compute-1.amazonaws.com dbname=d5bfmmilh08e9i user=weeyppzdvmawbl password=c0b534e93a7b15481529e16eda0cc19504b16834ecc472acbbfe9235603cd4b3"); 
-if (!$connection) { 
-  echo "Couldn't make a connection! ". pg_last_error(); 
+
+
+if (!$con) {
+	echo 'not connected to the server';
 }
-else {
-  echo 'connected to server';
+
+
+$firstName=$_POST['fname'];
+$lastName=$_POST['lname'];
+$Email=$_POST['email'];
+$Query=$_POST['query'];
+
+$sql="INSERT INTO FAQ (Firstname , Lastname , Email, Query) VALUES ('$firstName', '$lastName', '$Email','$Query')";
+
+if (!mysqli_query($con,$sql)) {
+	echo 'not inserted';
 }
-flush();
+
+else{
+	echo 'inserted';
+}
+
+header("refresh:5; url=index.php");
+
 ?>
