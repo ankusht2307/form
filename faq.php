a18cc5a146bd436da8c28cf85146d33e1bd696d9
ankusht2307 <ankusht2307@gmail.com>
2018-11-04 15:34:51 +0530

    testing db connection

--- faq.php
+++ faq.php
@@ -1,30 +1,15 @@
 <?php
+echo "<pre>If this line isn't printed you have a parse error in your script</pre>\n"; flush();
+error_reporting(E_ALL); ini_set('display_errors', true);    
+extension_loaded('pgsql') || die('pgsql module unavailable');
 
-$con=mysqli_connect('localhost','root','');
 
-if (!$con) {
-	echo 'not connected to the server';
+$connection = pg_connect("host=localhost dbname=mydb user=postgres password=pgsql"); 
+if (!$connection) { 
+  echo "Couldn't make a connection! ". pg_last_error(); 
 }
-
-if (!mysqli_select_db($con,'rashifaldotcom')) {
-	echo 'database not selected';
+else {
+  echo 'connected to server';
 }
-
-$firstName=$_POST['fname'];
-$lastName=$_POST['lname'];
-$Email=$_POST['email'];
-$Query=$_POST['query'];
-
-$sql="INSERT INTO FAQ (Firstname , Lastname , Email, Query) VALUES ('$firstName', '$lastName', '$Email','$Query')";
-
-if (!mysqli_query($con,$sql)) {
-	echo 'not inserted';
-}
-
-else{
-	echo 'inserted';
-}
-
-header("refresh:5; url=index.php");
-
+flush();
 ?>
