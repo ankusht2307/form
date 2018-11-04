d393b70e5177296966d9b8474083fea987345205
ankusht2307 <ankusht2307@gmail.com>
2018-11-04 15:39:18 +0530

    none

--- faq.php
+++ faq.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL); ini_set('display_errors', true);
 extension_loaded('pgsql') || die('pgsql module unavailable');
 
 
-$connection = pg_connect("host=localhost dbname=mydb user=postgres password=pgsql"); 
+$connection = pg_connect("host=ec2-23-21-171-249.compute-1.amazonaws.com dbname=d5bfmmilh08e9i user=weeyppzdvmawbl password=c0b534e93a7b15481529e16eda0cc19504b16834ecc472acbbfe9235603cd4b3"); 
 if (!$connection) { 
   echo "Couldn't make a connection! ". pg_last_error(); 
 }
