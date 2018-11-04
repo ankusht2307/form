8273953964c73553a7f9296dc256a5d0eea7291f
ankusht2307 <ankusht2307@gmail.com>
2018-11-04 15:58:36 +0530

    nothing

--- faq.php
+++ faq.php
@@ -3,7 +3,7 @@
 $connection = pg_connect("host=ec2-23-21-171-249.compute-1.amazonaws.com dbname=d5bfmmilh08e9i user=weeyppzdvmawbl password=c0b534e93a7b15481529e16eda0cc19504b16834ecc472acbbfe9235603cd4b3"); 
 
 
-if (!$con) {
+if (!$connection) {
 	echo 'not connected to the server';
 }
 
@@ -13,9 +13,9 @@ $lastName=$_POST['lname'];
 $Email=$_POST['email'];
 $Query=$_POST['query'];
 
-$sql="INSERT INTO FAQ (Firstname , Lastname , Email, Query) VALUES ('$firstName', '$lastName', '$Email','$Query')";
+$psql="INSERT INTO FAQ (Firstname , Lastname , Email, Query) VALUES ('$firstName', '$lastName', '$Email','$Query')";
 
-if (!mysqli_query($con,$sql)) {
+if (!mysqli_query($connection,$psql)) {
 	echo 'not inserted';
 }
 
